a788e9a3cfeb3fce0e5c32f67d87bf61686b1151
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-04-12 16:08:29 -0500

    Fetch data in parallel

---
+++ my-app/app/users/[userId]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Params = {
+  params: {
+    userId: string;
+  };
+};
+
+export default function UserPage({ params: { userId } }: Params) {
+  return <div></div>;
+}
