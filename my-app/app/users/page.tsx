f05be9a5c7fba285fac7aa5d919ebbf9a6599c5e
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-04-12 15:25:44 -0500

    started

---
+++ my-app/app/users/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import type { Metadata } from "next";
+import getAllUsers from "@/lib/getAllUsers";
+
+export const metadata: Metadata = {
+  title: "Users",
+};
+
+export default function UsersPage() {
+  return <div>page</div>;
+}
