224cb836abf3c81efd6a9a40519e91892dbeeb55
Gregory Harris <63122765+gregoryharris869@users.noreply.github.com>
2024-04-15 15:21:59 -0500

    updated

--- my-app/app/users/[userId]/page.tsx
+++ my-app/app/users/[userId]/page.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import getUser from "@/lib/getUser";
+import getUserPosts from "@/lib/getUserPosts";
+import { Suspense } from "react";
+import UserPosts from "./components/UserPosts";
 
 type Params = {
   params: {
@@ -6,6 +10,21 @@ type Params = {
   };
 };
 
-export default function UserPage({ params: { userId } }: Params) {
-  return <div></div>;
+export default async function UserPage({ params: { userId } }: Params) {
+  const userData: Promise<User> = getUser(userId);
+  const userPostsData: Promise<Post[]> = getUserPosts(userId);
+
+  // const [user, userPosts] = await Promise.all([userData, userPostsData]);
+
+  const user = await userData;
+
+  return (
+    <>
+      <h2>{user.name}</h2>
+      <br />
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <UserPosts promise={userPostsData} />
+      </Suspense>
+    </>
+  );
 }
